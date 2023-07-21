c86e7b7b05f9d71d1307d8b37a95fc4be015ec9b
StefanieLoeser <stefanie.loeser@gmx.net>
2023-07-21 12:23:10 +0200

    change path of images

--- pages/about.vue
+++ pages/about.vue
@@ -30,19 +30,19 @@
 const teamMembers = [
   {
     name: "Giancarlo",
-    img: "./assets/images/DSC2884.jpeg",
+    img: "./images/DSC2884.jpeg",
     intro:
       "Giancarlo (sier/sien) ist eine nette Person, die sich viel Mühe gibt, um netter zu werden. Sier arbeitet bei Special Circumstances, ein »Think-Tank für Forschung und Entwicklung«, wo Software mittels eines linguistischen Ansatzes verbessert werden kann. Giancarlo schreibt über Filme, liest viel, und ist Mitbegründer*in der Neurodiversen Gemeinschaft. Außerdem kommt Sier aus Peru, mag Katzen und forscht in Film, Medien und Kulturwissenschaft in Birkbeck, University of London. (GS)",
   },
   {
     name: "Katharina",
-    img: "./assets/images/Katharina_klein-768x1151.jpeg",
+    img: "./images/Katharina_klein-768x1151.jpeg",
     intro:
       "Katharina (they/sie) ist irgendwo zwischen reserviert und warmherzig. Sie verdient ihr Geld mit ihrem Wissen über Essen und mit einstudierten sozialen Umgangsformen – konkreter: sie arbeitet in einem Feinkostladen. Außerdem kocht und backt sie für Freund*innen und manchmal auch Fremde. Sie hat in Edinburgh studiert und in Berlin als Beraterin gearbeitet, kurze Zeit war sie auch Doktorandin. Sie ist Autist*in und hat chronische Migräne – letzteres nervt, ersteres ist cool. Jetzt ist sie Mitbegründer*in der Neurodiversen Gemeinschaft. (KB)",
   },
   {
     name: "Johanna",
-    img: "./assets/images/Johanna_klein-768x1151.jpeg",
+    img: "./images/Johanna_klein-768x1151.jpeg",
     intro:
       "Johanna (sie/they) ist eine Person, die gerne zu viel redet, singt und Wortwitze macht. Wenn sie nicht gerade mit allem überfordert, wütend und genervt ist und die Energie für soziale Interaktionen mal nicht fehlt, spielt sie Brettspiele mit Freund*innen und verbringt laute Abende in unangenehm verrauchten Bars. Sie hat Theater-, Film- , Medien- und Kulturwissenschaft in Wien und Berlin studiert und macht gerade ein Volontariat in der Hochschulkommunikation der HfS Ernst Busch. Bisher hat sie keine Diagnose, aber sie identifiziert sich als neurodivergent und ist Mitbegründer*in der Neurodiversen Gemeinschaft. (JM)",
   },
