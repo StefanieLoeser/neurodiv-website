b597acb2926009725cb7269372185aa0195b4d6e
StefanieLoeser <stefanie.loeser@gmx.net>
2023-07-23 14:31:20 +0200

    fix routing problem and refactor blog overview page

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="frame-s">
-    <h2 class="font-sans underline">{{ blogTitle }}</h2>
-    <p class="">{{ blogIntro }}</p>
-    <p class="font-sans text-sm text-gray-500">{{ blogDate }}</p>
-    <div v-for="tag in blogTags" class="">{{ tag }}</div>
+  <div class="frame-s flex flex-col gap-5 mb-10 p-5">
+    <div>
+      <h2 class="font-sans text-md font-bold underline pb-1">
+        {{ blogTitle }}
+      </h2>
+      <p class="font-sans pb-1">{{ blogIntro }}</p>
+      <p class="font-sans text-sm pb-1">{{ blogDate }}</p>
+      <div class="flex flex-wrap gap-2">
+        <div v-if="blogCategory" class="frame-s text-sm py-2 px-3 bg-orange">
+          {{ blogCategory }}
+        </div>
+        <div v-for="tag in blogTags" class="frame-s text-sm py-2 px-3">
+          {{ tag }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +24,7 @@ const props = defineProps({
   blogIntro: String,
   blogDate: String,
   blogTags: Array,
+  blogCategory: String,
 });
 </script>
 
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,40 +2,16 @@
   <div class="pb-10">
     <h2 class="text-head">Blog</h2>
 
-    <div
-      v-for="blog in blogPosts"
-      :key="blog.title"
-      class="frame-s flex flex-col gap-5 mb-10 p-5"
-    >
-      <div>
-        <h2 class="font-sans text-md font-bold underline">
-          {{ blog.title }}
-        </h2>
-        <p class="font-sans">{{ blog.intro }}</p>
-        <p class="font-sans text-sm">{{ blog.date }}</p>
-      </div>
-      <div class="flex flex-wrap gap-2">
-        <!-- :class="{ 'bg-orange': tags[0] }" -->
-        <div v-for="tag in blog.tags" class="frame-s text-sm py-2 px-3">
-          {{ tag }}
-        </div>
-      </div>
-    </div>
-
-    <!-- <div
-  
-      class="flex flex-col gap-10"
-    > -->
-    <!-- <BlogPost
+    <BlogPost
       v-for="blog in blogPosts"
       :key="blog.title"
       :blog-title="blog.title"
       :blog-date="blog.date"
       :blog-intro="blog.intro"
       :blog-tags="blog.tags"
-    /> -->
+      :blog-category="blog.category"
+    />
   </div>
-  <!-- </div> -->
 </template>
 
 <script setup>
@@ -45,21 +21,24 @@ const blogPosts = [
     date: "November 15, 2022",
     intro:
       "Ein satirischer Beitrag mit ernstgemeinten Tipps von Katharina und Giancarlo für Autist*innen",
-    tags: ["Work", "adhs", "ads", "ausbildung", "autismus", "neurodiversitt"],
+    category: "Work",
+    tags: ["adhs", "ads", "ausbildung", "autismus", "neurodiversität"],
   },
   {
     title: "Studium ist scheiße, mach das nicht!",
     date: "November 15, 2022",
     intro:
       "Ein satirischer Beitrag mit ernstgemeinten Tipps von Katharina und Giancarlo für Autist*innen",
-    tags: ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6"],
+    category: "Work",
+    tags: ["adhs", "ads", "ausbildung", "autismus", "neurodiversität"],
   },
   {
     title: "Studium ist scheiße, mach das nicht!",
     date: "November 15, 2022",
     intro:
       "Ein satirischer Beitrag mit ernstgemeinten Tipps von Katharina und Giancarlo für Autist*innen",
-    tags: ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6"],
+    category: "Work",
+    tags: ["adhs", "ads", "ausbildung", "autismus", "neurodiversität"],
   },
 ];
 </script>
