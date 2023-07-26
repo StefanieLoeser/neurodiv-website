e8ebd1dea53446ae43a50a75ff8a831974e37755
StefanieLoeser <stefanie.loeser@gmx.net>
2023-07-26 18:44:07 +0200

    start to build structure for individual posts

--- pages/blog/[...posts].vue
+++ pages/blog/[...posts].vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h2>Individual Post Page</h2>
+    <h2 class="text-head">{{ blogPost.title }}</h2>
+    <p class="font-sans">{{ blogPost.date }}</p>
+    <p class="font-sans">{{ blogPost.intro }}</p>
+    <div v-html="blogPost.text"></div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const blogPost = {
+  title: "Studium ist scheiße, mach das nicht!",
+  date: "November 15, 2022",
+  intro:
+    "Ein satirischer Beitrag mit ernstgemeinten Tipps von Katharina und Giancarlo für Autist*innen",
+  text: "Lorem ipsum dolor sit amet",
+  category: "Work",
+  tags: ["adhs", "ads", "ausbildung", "autismus", "neurodiversität"],
+};
 </script>
 
 <style></style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,6 +2,16 @@
   <div class="pb-10">
     <h2 class="text-head">Blog</h2>
 
+    <div>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <h2>{{ post.title }}</h2>
+          <div v-html="post.content"></div>
+          <p>{{ post.date }}</p>
+        </li>
+      </ul>
+    </div>
+
     <BlogPost
       v-for="blog in blogPosts"
       :key="blog.title"
@@ -15,6 +25,51 @@
 </template>
 
 <script setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+
+// const data = await $fetch(config.wordpressUrl, {
+//   method: "POST",
+//   body: {
+//     query: `
+
+// try to refactor fetch according to nuxt-graphql-client docs:
+// const { data } = await useAsyncGgl({
+//   operation: "query",
+//   body: {
+//     query: `
+//       query GetPosts {
+//         posts {
+//           nodes {
+//             id
+//             title
+//             content
+//           }
+//         }
+//       }
+//     `,
+//   },
+// });
+
+// onMounted(() => {
+//   console.log("fetched data", data);
+// });
+
+// const query = gql`
+//   query GetPosts {
+//     posts {
+//       nodes {
+//         id
+//         title
+//         content
+//       }
+//     }
+//   }
+// `;
+
+// const variables = { limit: 5 };
+// const { data } = await useAsyncQuery(query, variables);
+
 const blogPosts = [
   {
     title: "Studium ist scheiße, mach das nicht!",
